884f7f18bfcc460f38cada8ee532147eacd09cf2
TheNewtInTheSuit <nickpap9411@gmail.com>
2013-11-10 15:29:18 -0500

    Update elf_name_gen_example.py

--- elf_name_gen/elf_name_gen_example.py
+++ elf_name_gen/elf_name_gen_example.py
@@ -9,7 +9,7 @@ for i in range(10):
 	name   = random_elf_name()
 	gender = elf_name_grammatical_gender(name)
 
-	print(name, gender)
+	print("{}, {}".format(name, gender))
 
 print
 
@@ -20,4 +20,4 @@ for i in range(10):
 	if gender == 'neuter':
 		gender = resolve_gender_neutrality(name)
 		
-	print(name, gender)
+	print("{}, {}".format(name, gender))
